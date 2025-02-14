1e855b20ae707af7286fdf6922743c53131740a3
josephati <ja816@exeter.ac.uk>
2025-02-14 01:12:56 +0000

    Update fileHandling.py

--- Data Structures/fileHandling.py
+++ Data Structures/fileHandling.py
@@ -1,10 +1,7 @@
-import re
-
-is_numeric = lambda s: bool(re.fullmatch(r'\d+', s))
-
 def fileHandling(filename = "data.txt"):
     data = open("data.txt", "r")
     lines = data.readlines()
+    data.close()
 
     txt = lines[1].replace(',','')
     num = [int(s) for s in txt.split() if s.isdigit()]
@@ -14,9 +11,9 @@ def fileHandling(filename = "data.txt"):
     building = []
     for x in range(3, len(lines)):
         txt = lines[x].replace(',','')
+        txt = txt.strip("\n")
         requests = [int(s) for s in txt.split() if s.isdigit()]
         building.append(requests)
-    print(building)
 
 if __name__ == "__main__":
     fileHandling()
