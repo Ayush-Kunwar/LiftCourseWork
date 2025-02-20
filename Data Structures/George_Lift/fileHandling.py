a2f7ee5c07fc90967178a709b94551de60fb2d45
josephati <ja816@exeter.ac.uk>
2025-02-20 16:52:44 +0000

    Update fileHandling.py

--- Data Structures/George_Lift/fileHandling.py
+++ Data Structures/George_Lift/fileHandling.py
@@ -1,17 +1,19 @@
-import re
+def fileHandling(filename = "data.txt"):
+    data = open("data.txt", "r")
+    lines = data.readlines()
+    data.close()
 
-is_numeric = lambda s: bool(re.fullmatch(r'\d+', s))
-
-def fileHandling(filename):
-    data = open(filename, "r") #open the text file
-    lines = data.readlines() 
     txt = lines[1].replace(',','')
     num = [int(s) for s in txt.split() if s.isdigit()]
     floors = num[0]
     capacity = num[1]
+
     building = []
     for x in range(3, len(lines)):
         txt = lines[x].replace(',','')
+        txt = txt.strip("\n")
         requests = [int(s) for s in txt.split() if s.isdigit()]
         building.append(requests)
-    return building, capacity, floors
+
+if __name__ == "__main__":
+    fileHandling()
