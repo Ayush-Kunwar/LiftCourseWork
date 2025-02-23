d19408dc7e2e1b383e61e83dcd7752575d54565f
AshCham <ashleekchamisa@gmail.com>
2025-02-23 16:43:13 +0000

    Add files via upload
    Updated File Handling and Generate Files Method

--- Data Structures/fileHandling.py
+++ Data Structures/fileHandling.py
@@ -1,19 +1,44 @@
-def fileHandling(filename = "data.txt"):
-    data = open("data.txt", "r")
-    lines = data.readlines()
-    data.close()
+import re
 
-    txt = lines[1].replace(',','')
-    num = [int(s) for s in txt.split() if s.isdigit()]
-    floors = num[0]
-    capacity = num[1]
+is_numeric = lambda s: bool(re.fullmatch(r'\d+', s))
 
-    building = []
-    for x in range(3, len(lines)):
-        txt = lines[x].replace(',','')
-        txt = txt.strip("\n")
-        requests = [int(s) for s in txt.split() if s.isdigit()]
-        building.append(requests)
+def fileHandle(filename):
+    try:
+        data = open(filename, "r") #open the text file
+        lines = data.readlines()
+        if len(lines) < 3:
+            raise ValueError("ERROR: File is too short and is missing details")
+        txt = lines[1].strip().replace(',','')
+        if not txt.replace(" ","").isdigit():
+            raise ValueError(f"ERROR: Number of Floors and Capacity is not defined properly")
+        
+        num = [int(s) for s in txt.split()]
+        floors = num[0]
+        capacity = num[1]
+        
+        if floors < 2:
+            raise ValueError("ERROR: There must be at least 2 floors")
+        if capacity < 1:
+            raise ValueError("ERROR: Lift capacity must be atleast be 1")
+        
+        building = []
+        for x in range(3, len(lines)):
+            txt = lines[x].replace(',','')
+            if not txt.strip().replace(":","").replace(" ","").isdigit():
+                raise ValueError(f"ERROR: Invalid Floor Requests format")
+            
+            requests = [int(s) for s in txt.split() if s.isdigit()]
+            current_floor = x - 2  # Since floors start from line 3, floor number is (x - 2)
 
-if __name__ == "__main__":
-    fileHandling()
+            if any(request == current_floor for request in requests):
+                raise ValueError(f"ERROR: Requested floor is the same as the current floor: {current_floor}")
+            if any(request < 1 or request > floors for request in requests):
+                raise ValueError(f"ERROR: Requested floor is out of building's range (floor: {current_floor})")
+    
+            building.append(requests)
+        data.close()
+        return building, capacity, floors
+    
+    except FileNotFoundError:
+        print(f"ERROR: File '{filename}' not found")
+    
